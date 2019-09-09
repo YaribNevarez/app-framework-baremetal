ae5f921648a040a6aad2476fbf53fa5184c0307d
YaribNevarez <yarib_007@hotmail.com>
2019-09-09 13:52:15 +0200

    Template

---
+++ apps/sbs_app/inc/sbs_app.h
@@ -0,0 +1,35 @@
+//------------------------------------------------------------------------------
+/**
+ *
+ * @file: sbs_app.h
+ *
+ * @Created on: September 9th, 2019
+ * @Author: Yarib Nevarez
+ *
+ *
+ * @brief - Spike by Spike Neural Network test application
+ * <Requirement Doc Reference>
+ * <Design Doc Reference>
+ *
+ * @copyright Copyright [2019] Institute for Theoretical Electrical Engineering 
+ *                             and Microelectronics (ITEM)
+ * All Rights Reserved.
+ *
+ */
+//------------------------------------------------------------------------------
+
+// IFNDEF ----------------------------------------------------------------------
+#ifndef SBS_APP_H_
+#define SBS_APP_H_
+
+// INCLUDES --------------------------------------------------------------------
+
+// FORWARD DECLARATIONS --------------------------------------------------------
+
+// TYPEDEFS AND DEFINES --------------------------------------------------------
+
+// EUNUMERATIONS ---------------------------------------------------------------
+
+// DECLARATIONS ----------------------------------------------------------------
+
+#endif /* SBS_APP_H_ */
---
+++ apps/sbs_app/src/sbs_app.c
@@ -0,0 +1,32 @@
+//------------------------------------------------------------------------------
+/**
+ *
+ * @file: sbs_app.c
+ *
+ * @Created on: September 9th, 2019
+ * @Author: Yarib Nevarez
+ *
+ *
+ * @brief - Spike by Spike Neural Network test application
+ * <Requirement Doc Reference>
+ * <Design Doc Reference>
+ *
+ * @copyright Copyright [2019] Institute for Theoretical Electrical Engineering
+ *                             and Microelectronics (ITEM)
+ * All Rights Reserved.
+ *
+ *
+ */
+//------------------------------------------------------------------------------
+// INCLUDES --------------------------------------------------------------------
+
+// FORWARD DECLARATIONS --------------------------------------------------------
+
+// TYPEDEFS AND DEFINES --------------------------------------------------------
+
+// EUNUMERATIONS ---------------------------------------------------------------
+
+// STRUCTS AND NAMESPACES ------------------------------------------------------
+
+// DEFINITIONs -----------------------------------------------------------------
+
