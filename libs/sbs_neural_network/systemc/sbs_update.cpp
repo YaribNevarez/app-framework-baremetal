146853a5cbadaf5055acad394cc57e3bceed6645
YaribNevarez <yarib_007@hotmail.com>
2019-10-13 09:22:39 -0500

    Use of hls::stream in SystemC accelerator

--- libs/sbs_neural_network/systemc/sbs_update.cpp
+++ libs/sbs_neural_network/systemc/sbs_update.cpp
@@ -4,22 +4,25 @@
 
 typedef union
 {
-	unsigned int u32;
-	float        f32;
+  unsigned int u32;
+  float f32;
 } FloatToInt;
 
 #define MAX_LAYER_SIZE      (28*28)
 #define MAX_KERNEL_SIZE     (5*5)
 #define MAX_VECTOR_SIZE     (1024)
 
-typedef ap_axis<32,2,5,6> StreamChannel;
+#define NEGLECTING_CONSTANT (1e-20)
 
-void sbs_update(StreamChannel stream_in[MAX_LAYER_SIZE * (MAX_KERNEL_SIZE + 1)* MAX_VECTOR_SIZE],
-				StreamChannel stream_out[MAX_LAYER_SIZE * MAX_VECTOR_SIZE],
-				int layerSize,
-				int kernelSize,
-				int vectorSize,
-				float epsilon)
+typedef ap_axis<32, 2, 5, 6> StreamChannel;
+
+
+void sbs_update (hls::stream<StreamChannel> &stream_in,
+                 hls::stream<StreamChannel> &stream_out,
+                 int layerSize,
+                 int kernelSize,
+                 int vectorSize,
+                 float epsilon)
 {
 #pragma HLS INTERFACE axis      port=stream_in
 #pragma HLS INTERFACE axis      port=stream_out
@@ -31,9 +34,8 @@ void sbs_update(StreamChannel stream_in[MAX_LAYER_SIZE * (MAX_KERNEL_SIZE + 1)* 
 
   static int ip_index;
   static int i;
-  static int j = 0;
   static int batch;
-  static StreamChannel last;
+  static StreamChannel channel;
   static float state_vector[MAX_VECTOR_SIZE];
   static float temp_data[MAX_VECTOR_SIZE];
   static float epsion_over_sum;
@@ -43,58 +45,61 @@ void sbs_update(StreamChannel stream_in[MAX_LAYER_SIZE * (MAX_KERNEL_SIZE + 1)* 
   float reverse_epsilon = 1.0f / (1.0f + epsilon);
   static float sum;
 
-  for (ip_index = 0; ip_index < layerSize; ip_index ++)
+  bool read_channel = true;
+
+  for (ip_index = 0; ip_index < layerSize; ip_index++)
   {
 #pragma HLS pipeline
-	  for (i = 0; i < vectorSize; i++)
-	  {
-	#pragma HLS pipeline
-		  if (i == 0)
-		  {
-			  last = stream_in[i];
-			  float_to_int.u32 = last.data;
-		  }
-		  else
-		  {
-			  float_to_int.u32 = stream_in[i].data;
-		  }
+    for (i = 0; i < vectorSize; i++)
+    {
+#pragma HLS pipeline
+      if (read_channel)
+      {
+        channel = stream_in.read ();
+        float_to_int.u32 = channel.data;
+        state_vector[i] = float_to_int.f32;
+        read_channel = false;
+      }
+      else
+      {
+        float_to_int.u32 = stream_in.read ().data;
+        state_vector[i] = float_to_int.f32;
+      }
+    }
 
-		state_vector[i] = float_to_int.f32;
-	  }
+    for (batch = 0; batch < kernelSize; batch++)
+    {
+#pragma HLS pipeline
+      sum = 0.0f;
+      for (i = 0; i < vectorSize; i++)
+      {
+#pragma HLS pipeline
+        float_to_int.u32 = stream_in.read ().data;
+        temp_data[i] = state_vector[i] * float_to_int.f32;
+        sum += temp_data[i];
+      }
 
-	  for (batch = 0; batch < kernelSize; batch++)
-	  {
-	#pragma HLS pipeline
-		j = vectorSize * (1 + batch);
-		sum = 0.0f;
-		for (i = 0; i < vectorSize; i++)
-		{
-	#pragma HLS pipeline
-		  float_to_int.u32 = stream_in[i + j].data;
-		  temp_data[i] = state_vector[i] * float_to_int.f32;
-		  sum += temp_data[i];
-		}
+      if (NEGLECTING_CONSTANT < sum)
+      {
+        epsion_over_sum = epsilon / sum;
+        for (i = 0; i < vectorSize; i++)
+        {
+#pragma HLS pipeline
+          state_vector[i] = reverse_epsilon
+              * (state_vector[i] + temp_data[i] * epsion_over_sum);
+        }
+      }
+    }
 
-		epsion_over_sum = epsilon / sum;
+    for (i = 0; i < vectorSize; i++)
+    {
+#pragma HLS pipeline
+      float_to_int.f32 = state_vector[i];
 
-		if (sum > 1e-20) // TODO: DEFINE constant
-		  for (i = 0; i < vectorSize; i++)
-		  {
-	#pragma HLS pipeline
-			state_vector[i] = reverse_epsilon * (state_vector[i] + temp_data[i] * epsion_over_sum);
-		  }
-	  }
+      channel.data = float_to_int.u32;
+      channel.last = (i == vectorSize - 1) && (ip_index == layerSize - 1);
 
-	  for (i = 0; i < vectorSize; i++)
-	  {
-	#pragma HLS pipeline
-		float_to_int.f32 = state_vector[i];
-
-		last.data = float_to_int.u32;
-		last.last = (i == vectorSize - 1) && (ip_index == layerSize - 1);
-
-		stream_out[i] = last;
-	  }
+      stream_out.write(channel);
+    }
   }
 }
-
