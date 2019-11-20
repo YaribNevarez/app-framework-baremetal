8c7de8ac9222449a2c06a8d5677726bd6daa8cd0
YaribNevarez <yarib_007@hotmail.com>
2019-11-20 19:19:32 +0100

    Customizable hardware acceleration

--- apps/sbs_helper/inc/sbs_helper_app.h
+++ apps/sbs_helper/inc/sbs_helper_app.h
@@ -1,9 +1,9 @@
 //------------------------------------------------------------------------------
 /**
  *
- * @file: sbs_app_config.h
+ * @file: sbs_app.h
  *
- * @Created on: September 15th, 2019
+ * @Created on: September 9th, 2019
  * @Author: Yarib Nevarez
  *
  *
@@ -11,38 +11,41 @@
  * <Requirement Doc Reference>
  * <Design Doc Reference>
  *
- * @copyright Copyright [2019] Institute for Theoretical Electrical Engineering
+ * @copyright Copyright [2019] Institute for Theoretical Electrical Engineering 
  *                             and Microelectronics (ITEM)
  * All Rights Reserved.
  *
  */
 //------------------------------------------------------------------------------
+
 // IFNDEF ----------------------------------------------------------------------
-#ifndef SBS_APP_CONFIG_H_
-#define SBS_APP_CONFIG_H_
+#ifndef SBS_HELPER_APP_H_
+#define SBS_HELPER_APP_H_
 
 // INCLUDES --------------------------------------------------------------------
+//#include "xil_types.h"
+#include "stdint.h"
+#include "stddef.h"
 
+#include "sbs_helper_app_config.h"
+
+#include "result.h"
 // FORWARD DECLARATIONS --------------------------------------------------------
 
 // TYPEDEFS AND DEFINES --------------------------------------------------------
-//#define USE_XILINX
-
-#define SBS_INPUT_PATTERN_FILE   "/MNIST/Pattern/Input_1.bin"
-
-#define SBS_INPUT_PATTERN_FORMAT_NAME "/MNIST/Pattern/Input_%d.bin"
-#define SBS_INPUT_PATTERN_FIRST       1
-#define SBS_INPUT_PATTERN_LAST        50
-
-#define SBS_P_IN_H1_WEIGHTS_FILE "/MNIST/W_X_H1.bin"
-#define SBS_P_H1_H2_WEIGHTS_FILE "/MNIST/W_H1_H2.bin"
-#define SBS_P_H2_H3_WEIGHTS_FILE "/MNIST/W_H2_H3.bin"
-#define SBS_P_H3_H4_WEIGHTS_FILE "/MNIST/W_H3_H4.bin"
-#define SBS_P_H4_H5_WEIGHTS_FILE "/MNIST/W_H4_H5.bin"
-#define SBS_P_H5_HY_WEIGHTS_FILE "/MNIST/W_H5_HY.bin"
 
 // EUNUMERATIONS ---------------------------------------------------------------
 
 // DECLARATIONS ----------------------------------------------------------------
 
-#endif /* SBS_APP_CONFIG_H_ */
+typedef struct _SbsHelperApp SbsHelperApp;
+
+struct _SbsHelperApp
+{
+  Result  (* initialize)(SbsHelperApp *);
+  Result  (* run)(SbsHelperApp *);
+  void    (* dispose)(SbsHelperApp **);
+};
+
+SbsHelperApp * SbsHelperApp_new(void);
+#endif /* SBS_HELPER_APP_H_ */
