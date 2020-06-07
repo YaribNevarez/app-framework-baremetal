72cfdc034aad366114802d4d31b9730fd54447db
YaribNevarez <yarib_007@hotmail.com>
2020-06-07 12:26:07 -0500

    Accelerator for 32Bit channel

--- libs/sbs_neural_network/systemc/sbs_accelerator_64.cpp
+++ libs/sbs_neural_network/systemc/sbs_accelerator_64.cpp
@@ -38,51 +38,62 @@ static unsigned int mti=N+1; /* mti==N+1 means mt[N] is not initialized */
 /* initializing the array with a NONZERO seed */
 static void MT19937_sgenrand (unsigned int instance, unsigned int seed)
 {
-    /* setting initial seeds to mt[N] using         */
-    /* the generator Line 25 of Table 1 in          */
-    /* [KNUTH 1981, The Art of Computer Programming */
-    /*    Vol. 2 (2nd Ed.), pp102]                  */
-    mt[0]= seed & 0xffffffff;
-    for (mti=1; mti<N; mti++)
-        mt[mti] = (69069 * mt[mti-1]) & 0xffffffff;
+  /* setting initial seeds to mt[N] using         */
+  /* the generator Line 25 of Table 1 in          */
+  /* [KNUTH 1981, The Art of Computer Programming */
+  /*    Vol. 2 (2nd Ed.), pp102]                  */
+  mt[0] = seed & 0xffffffff;
+  for (mti = 1; mti < N; mti++)
+  {
+    mt[mti] = (69069 * mt[mti - 1]) & 0xffffffff;
+  }
 
-    MT19937_flags_ |= INITIALIZED;
+  MT19937_flags_ |= INITIALIZED;
 }
 
 static unsigned int MT19937_rand (unsigned int instance)
 {
 #pragma HLS inline off
-    unsigned int y;
-    static unsigned int mag01[2]={0x0, MATRIX_A};
-    /* mag01[x] = x * MATRIX_A  for x=0,1 */
+  unsigned int y;
+  static unsigned int mag01[2] = { 0x0, MATRIX_A };
+  /* mag01[x] = x * MATRIX_A  for x=0,1 */
 
-    if (mti >= N) { /* generate N words at one time */
-        int kk;
+  if (mti >= N)
+  { /* generate N words at one time */
+    int kk;
 
-        if (mti == N+1)   /* if sgenrand() has not been called, */
-          MT19937_sgenrand(instance, 4357); /* a default initial seed is used   */
-
-        for (kk=0;kk<N-M;kk++) {
-            y = (mt[kk]&UPPER_MASK)|(mt[kk+1]&LOWER_MASK);
-            mt[kk] = mt[kk+M] ^ (y >> 1) ^ mag01[y & 0x1];
-        }
-        for (;kk<N-1;kk++) {
-            y = (mt[kk]&UPPER_MASK)|(mt[kk+1]&LOWER_MASK);
-            mt[kk] = mt[kk+(M-N)] ^ (y >> 1) ^ mag01[y & 0x1];
-        }
-        y = (mt[N-1]&UPPER_MASK)|(mt[0]&LOWER_MASK);
-        mt[N-1] = mt[M-1] ^ (y >> 1) ^ mag01[y & 0x1];
-
-        mti = 0;
+    if (mti == N + 1)
+    {/* if sgenrand() has not been called, */
+      MT19937_sgenrand (instance, 4357); /* a default initial seed is used   */
     }
 
-    y = mt[mti++];
-    y ^= TEMPERING_SHIFT_U(y);
-    y ^= TEMPERING_SHIFT_S(y) & TEMPERING_MASK_B;
-    y ^= TEMPERING_SHIFT_T(y) & TEMPERING_MASK_C;
-    y ^= TEMPERING_SHIFT_L(y);
+    for (kk = 0; kk < N - M; kk++)
+    {
+#pragma HLS unroll factor=32
+      y = (mt[kk] & UPPER_MASK) | (mt[kk + 1] & LOWER_MASK);
+      mt[kk] = mt[kk + M] ^ (y >> 1) ^ mag01[y & 0x1];
+    }
 
-    return y;
+    for (; kk < N - 1; kk++)
+    {
+#pragma HLS unroll factor=32
+      y = (mt[kk] & UPPER_MASK) | (mt[kk + 1] & LOWER_MASK);
+      mt[kk] = mt[kk + (M - N)] ^ (y >> 1) ^ mag01[y & 0x1];
+    }
+
+    y = (mt[N - 1] & UPPER_MASK) | (mt[0] & LOWER_MASK);
+    mt[N - 1] = mt[M - 1] ^ (y >> 1) ^ mag01[y & 0x1];
+
+    mti = 0;
+  }
+
+  y = mt[mti++];
+  y ^= TEMPERING_SHIFT_U(y);
+  y ^= TEMPERING_SHIFT_S(y) & TEMPERING_MASK_B;
+  y ^= TEMPERING_SHIFT_T(y) & TEMPERING_MASK_C;
+  y ^= TEMPERING_SHIFT_L(y);
+
+  return y;
 }
 
 
@@ -205,7 +216,7 @@ unsigned int sbs_accelerator_64 (hls::stream<StreamChannel> &stream_in,
 #pragma HLS pipeline
       for (int i = 0; i < vectorSize; i += (CHANNEL_WIDTH / WEIGHT_VECTOR_WIDTH))
       {
-  #pragma HLS pipeline
+#pragma HLS pipeline
         input = stream_in.read ().data;
 
         for (int hw = 0; hw <= CHANNEL_WIDTH / WEIGHT_VECTOR_WIDTH; hw++)
@@ -245,7 +256,7 @@ unsigned int sbs_accelerator_64 (hls::stream<StreamChannel> &stream_in,
 #pragma HLS pipeline
       channel.data = 0;
 
-      for (int hw = 0; hw <= CHANNEL_WIDTH / WEIGHT_VECTOR_WIDTH; hw++)
+      for (int hw = 0; hw <= CHANNEL_WIDTH / STATE_VECTOR_WIDTH; hw++)
       {
 #pragma HLS unroll
         if (i + hw < vectorSize)
